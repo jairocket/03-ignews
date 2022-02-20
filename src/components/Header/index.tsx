b54f8e071f1e51e4d434f8e591ceebb7938c17fa
jairocket <jailsonanjos@hotmail.com>
2022-02-20 11:43:48 -0300

    componente signinbutton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header(){
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ignewa" />
+                
+                <img src="/images/logo.svg" alt="ignews" />
                 <nav>
                     <a className={styles.active}>Home</a>
                     <a>Posts</a>
                 </nav>
+                <SignInButton />
             </div>
         </header>
     );
