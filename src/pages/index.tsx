815fedcef1bc775b4d0377098068471c679895b0
jairocket <jailsonanjos@hotmail.com>
2022-02-22 19:49:17 -0300

    componente SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import  Head  from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 //import Image from 'next/image'
 import styles from  './home.module.scss'
 
@@ -17,6 +18,7 @@ export default function Home() {
             Get access to all the publications <br/>
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
