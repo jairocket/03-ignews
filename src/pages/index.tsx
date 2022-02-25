9f18a45dc6299bac54611451b08ea2666fe1550a
jairocket <jailsonanjos@hotmail.com>
2022-02-24 22:30:22 -0300

    server site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import  Head  from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 //import Image from 'next/image'
 import styles from  './home.module.scss'
 import { stripe } from '../services/stripe'
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () =>{
+export const getStaticProps: GetStaticProps = async () =>{
   const price = await stripe.prices.retrieve('price_1KWrHIBHN0SXEGf4fbIykeef', {
     expand: ['product']
   });
@@ -52,7 +52,8 @@ export const getServerSideProps: GetServerSideProps = async () =>{
   return {
     props:{
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 //24horas
   }
 
 }
