cfe3741ec05f8f0163f372c8c8b68bff4feb76e8
jairocket <jailsonanjos@hotmail.com>
2022-03-01 18:58:54 -0300

    salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 
 import NextAuth from 'next-auth'
 import  GitHubProvider from 'next-auth/providers/github'
+import { query as q } from 'faunadb'
+import { signIn } from 'next-auth/react'
+import { fauna } from '../../../services/fauna'
+import { FaUnderline } from 'react-icons/fa'
 
 export default NextAuth({
     secret: process.env.SECRET,
@@ -12,5 +16,41 @@ export default NextAuth({
         }),
     ],
   
+    callbacks: {
+        async signIn({user, account, profile, email, credentials}){
+            const mail = user.email
+            try{
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            {data: {mail}}
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+                return true
+            }catch{
+                return false
+            }
+            
+            
+        }
+    }
+  
+    
 
 }) 
